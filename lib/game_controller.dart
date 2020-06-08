495e0bba864f5f3468debaa101864e3b5de76ce9
Ahd <dafuq.ahd@gmail.com>
2020-06-08 07:35:15 +0300

    finish tutorial flutter game

---
+++ lib/game_controller.dart
@@ -0,0 +1,120 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutgame/components/enemy.dart';
+import 'package:flutgame/components/healthbar.dart';
+import 'package:flutgame/components/highscore_text.dart';
+import 'package:flutgame/components/player.dart';
+import 'package:flutgame/components/score_text.dart';
+import 'package:flutgame/components/start_text.dart';
+import 'package:flutgame/enemy_spawner.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutgame/state.dart';
+
+class GameController extends Game {
+  final SharedPreferences storage;
+  Random rand;
+  Size screenSize;
+  double tileSize;
+  Player player;
+  EnemySpawner enemySpawner;
+  List<Enemy> enemies;
+  HealthBar healthBar;
+  int score;
+  ScoreText scoreText;
+  MyState state;
+  HighscoreText highscoreText;
+  StartText startText;
+  GameController(this.storage) {
+    initialize();
+  }
+  void initialize() async {
+    resize(await Flame.util.initialDimensions());
+    state = MyState.menu;
+    rand = Random();
+    player = Player(this);
+    enemies = List<Enemy>();
+    enemySpawner = EnemySpawner(this);
+    healthBar = HealthBar(this);
+    score = 0;
+    scoreText = ScoreText(this);
+    highscoreText = HighscoreText(this);
+    startText = StartText(this);
+  }
+
+  void render(Canvas c) {
+    Rect background = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
+    Paint backgroundPaint = Paint()..color = Color(0xFFFAFAFA);
+    c.drawRect(background, backgroundPaint);
+    player.render(c);
+    if (state == MyState.menu) {
+      startText.render(c);
+      highscoreText.render(c);
+    } else if (state == MyState.playing) {
+      enemies.forEach((Enemy enemy) => enemy.render(c));
+      scoreText.render(c);
+      healthBar.render(c);
+    }
+  }
+
+  void update(double t) {
+    if (state == MyState.menu) {
+      startText.update(t);
+      highscoreText.update(t);
+    } else if (state == MyState.playing) {
+      enemySpawner.update(t);
+      enemies.forEach((Enemy enemy) => enemy.update(t));
+      enemies.removeWhere((Enemy enemy) => enemy.isDead);
+      player.update(t);
+      scoreText.update(t);
+      healthBar.update(t);
+    }
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    tileSize = screenSize.width / 10;
+  }
+
+  void onTapDown(TapDownDetails d) {
+    if (state == MyState.menu) {
+      state = MyState.playing;
+    } else if (state == MyState.playing) {
+      enemies.forEach((Enemy enemy) {
+        if (enemy.enemyRect.contains(d.globalPosition)) {
+          enemy.onTapDown();
+        }
+      });
+    }
+  }
+
+  void spawnEnemy() {
+    double x, y;
+    switch (rand.nextInt(4)) {
+      case 0:
+        //top
+        x = rand.nextDouble() * screenSize.width;
+        y = -tileSize * 2.5;
+        break;
+      case 1:
+        //right
+        x = screenSize.width + tileSize * 2.5;
+        y = rand.nextDouble() * screenSize.height;
+        break;
+      case 2:
+        //bottom
+        x = rand.nextDouble() * screenSize.width;
+        y = screenSize.height + tileSize * 2.5;
+        break;
+      case 3:
+        // left
+        x = -tileSize * 2.5;
+        y = rand.nextDouble() * screenSize.height;
+        break;
+    }
+    enemies.add(Enemy(this, x, y));
+  }
+}
